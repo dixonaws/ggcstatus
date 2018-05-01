9f5cfdd7196ad60ab74ddfbc7cc548e978c395de
dixonaws <dixonaws@amazon.com>
2018-04-30 21:08:41 -0400

    modified setvin.py to read from a configuration file

--- setvin.py
+++ setvin.py
@@ -4,7 +4,18 @@ import memcache
 
 mc = memcache.Client(['127.0.0.1:11211'], debug=0)
 
-# Set the vehicle VIN in a memache key
-mc.set('VIN', 'WBA3B9G59ENR92112')
+# open the ccdtw configuration file, read the VIN there, and close
+fileVinConfig=open('/etc/ccdtw/vehicle_vin.conf', 'r')
 
+strVin=fileVinConfig.read()
+fileVinConfig.close()
+
+
+print 'ccdtw configuration: /etc/ccdtw/vehicle_vin.conf has VIN: ' + strVin 
+
+# Set the vehicle VIN in a memache key
+mc.set('VIN', strVin)
+
+# Retrieve the vehicle VIN from memcache
 print 'Vehicle VIN: ' + mc.get('VIN')
+
